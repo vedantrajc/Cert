8879fe5868eb005851fb222c1758527ed88dd9cc
Vedant <vedant_raj@yahoo.co.in>
2023-10-21 17:22:13 +0530

    livw cosw

---
+++ cert.js
@@ -0,0 +1,8 @@
+const generatePDF = async (content)=>{
+
+    // Save PDF-LIB functions in a variable
+    const {PDFDocument, rgb} = pdfLib;
+
+    // Fectch Existing PDF data into an Array
+    
+}
