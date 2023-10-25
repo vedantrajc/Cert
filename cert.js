58011e64c40ad8fe40d7fdd6a03dfef086c4f03e
Vedant <vedant_raj@yahoo.co.in>
2023-10-25 21:44:16 +0530

    Working

--- cert.js
+++ cert.js
@@ -1,8 +1,57 @@
+
+
+// Import PDFDocument from PDFLib
+const {PDFDocument} = PDFLib;
+
 const generatePDF = async (content)=>{
 
-    // Save PDF-LIB functions in a variable
-    const {PDFDocument, rgb} = pdfLib;
+    // Fectch PDF backgroud data into an Array
+    const bgPdfBytes = await fetch("CertificateBG.pdf").then(res => res.arrayBuffer());
 
-    // Fectch Existing PDF data into an Array
+    // Fetch Font
+    const centFontBytes = await fetch("CENTURY.ttf").then(res => res.arrayBuffer());
+
+    // Load a PDF with background data
+    const pdfDoc = await PDFDocument.load(bgPdfBytes);
+
+   // Register the `fontkit` instance
+   pdfDoc.registerFontkit(fontkit);
+
+    // Embed font to pdfDoc
+    const centFont = await pdfDoc.embedFont(centFontBytes);
+
     
-}
+    // Get total number of pages from PDF doc
+    const pages = pdfDoc.getPages();
+
+    //Select first page
+    const firstPage = pages[0];
+
+    const {width,height} = firstPage.getSize();
+
+    // Edit First Page
+    firstPage.drawText(content, {
+
+        // Starting POSITION of the content can be modified from here
+        x: 70,
+        y: 300,
+        
+        maxWidth: width-130,
+        size: 14,
+        font: centFont
+    });
+
+    
+    // Convert pdfDoc to Data URI
+    const pdfDataUri = await pdfDoc.saveAsBase64({dataUri: true});
+
+    document.querySelector("#iframe").src = pdfDataUri;
+
+    // Serialize the PDFDocument to bytes (a Uint8Array)
+    const pdfBytes = await pdfDoc.save();
+
+    // Trigger the browser to download the PDF document
+    //download(pdfBytes, "pdf-lib_creation_example.pdf", "application/pdf");
+};
+
+generatePDF($content)
---
+++ content.js
@@ -0,0 +1,5 @@
+$articalName = "AYURVEDIC MANAGEMENT OF RHEUMATOID ARTHRITIS: A CASE STUDY";
+$manuscriptNo = "WJPMR/6656/9/2023";
+$authors = "Dr. Shraddha Saraf, Dr. Swati Nagpal, Dr. Rajesh Meshram and Dr. Shwetal Shivhare";
+
+$content = "This is to certify that Article entitled \“".concat($articalName , "\", Manuscript no. " , $manuscriptNo , ", by " , $authors , ", has been published in World Journal of Pharmaceutical and Medical Research, (Volume 9, Issue 10.) after getting reviewed by three reviewers.");
